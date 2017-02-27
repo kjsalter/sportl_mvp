09317ea20f0460710711d8593bab0935fb04446a
Catherine Oxley <catherine.oxley@live.co.uk>
2017-02-27 13:50:20 +0000

    pundit ready

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,18 +1,28 @@
 class EventsController < ApplicationController
   before_action :set_event
-
+  # list all animals
   def index
+    @events = Event.all
   end
 
+  # create a new animal
   def new
+    @event = Event.new
   end
 
+  # perform create action
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to root_path
+    end
   end
 
+  # display particular event
   def show
   end
 
+  # edit event details
   def edit
   end
 
