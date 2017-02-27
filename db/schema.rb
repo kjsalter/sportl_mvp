f2582f3acce48e18047c5cc26b8d95b85630f68c
Archie Gilbert <archiegilbert@me.com>
2017-02-27 16:47:09 +0000

    Schema changed from fb

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,11 @@ ActiveRecord::Schema.define(version: 20170227153752) do
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.integer  "booking_state"
+    t.integer  "booking_state", default: 0
     t.integer  "user_id"
     t.integer  "event_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
     t.index ["event_id"], name: "index_bookings_on_event_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
