1e476aa6228cf9a4cd8574b6ffba84d8c332159a
Archie Gilbert <archiegilbert@me.com>
2017-02-28 16:03:34 +0000

    maps are fixed in production I think now :* soz

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -28,7 +28,7 @@ class EventsController < ApplicationController
 
     @event.user = current_user
     if @event.save
-      redirect_to events_path
+      redirect_to event_path(@event)
     else
       render :new
     end
