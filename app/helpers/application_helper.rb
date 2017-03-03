7aa956bc835100720bd9cb927f0a8ea21f61e74b
Katie Salter <katie.j.salter@gmail.com>
2017-03-03 17:02:11 +0000

    hopefully working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,12 +9,12 @@ module ApplicationHelper
     opts[:alt] = user.first_name
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}?s=#{opts.delete(:size) { 40 }}",
               opts
+  end
 
   def levels_list
     levels = %w(Any 1-beginner 2 3 4 5-advanced)
     levels.map.with_index do |lvl, index|
       [lvl, index]
     end
-
   end
 end
